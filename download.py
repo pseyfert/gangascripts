03d90047480517a39c13566a248707dce7e38bc6
Paul Seyfert <pseyfert@physi.uni-heidelberg.de>
2013-07-23 10:16:52 +0200

    use variable name sj for subjobs instead of j

--- download.py
+++ download.py
@@ -18,24 +18,24 @@ def download(job,min=-1,targetdir=None,force_redownload=False):
     else :
        thejob = job
 
-    for j in thejob.subjobs:
-        if j.status=="completed" and j.id>min:
-            for f in j.outputfiles.get("*.root"):
+    for sj in thejob.subjobs.select(status="completed"):
+        if sj.id>min:
+            for f in sj.outputfiles.get("*.root"):
                 if ""==f.lfn:
-                    print "no lfn for file ", f.namePattern, " from job", str(thejob.id), ".", str(j.id)
+                    print "no lfn for file ", f.namePattern, " from job", str(thejob.id), ".", str(sj.id)
                     continue
                 if isinstance(targetdir,str):
                   f.localDir = targetdir
                   ending = re.sub(".*\.","",f.namePattern)  # the file ending is everything after the last .
                   mainpart = re.sub("\."+ending,"",f.namePattern) # the main part of the filename is everything before the ending (removing the .)
-                  newmainpart = mainpart + "_" + str(j.id)
+                  newmainpart = mainpart + "_" + str(sj.id)
                   targetfilename = targetdir + "/" + newmainpart + "." + ending
                   automaticname = f.localDir + "/" + f.namePattern
                 else:
                   automaticname = f.outputdir + f.namePattern
                   targetfilename = f.outputdir + f.namePattern
                 if (os.path.isfile(targetfilename)) :
-                    print "file ", f.namePattern, " from job", str(thejob.id), ".", str(j.id), " already loaded"
+                    print "file ", f.namePattern, " from job", str(thejob.id), ".", str(sj.id), " already loaded"
                     if force_redownload:
                         print "load it anyway"
                         f.get()
