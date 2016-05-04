23d9481c13c172535be8b17143dceb88d9247f3f
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-05-04 15:46:13 +0200

    draft postprocessing of lfn file

--- writelfntofile.py
+++ writelfntofile.py
@@ -13,5 +13,8 @@ def write_lfn_to_txt(jobidlist):
                     else:
                         f.write(of.lfn)
                         f.write("\n")
+    # draft
+    #import subprocess
+    #subprocess.call("dirac-dms-lfn-accessURL "+jobs(jobidlist[0]).name +'.txt CERN-USER | grep eoslhcb | sed "s/ *$//" | sed "s/.* //"[ >> '+jobs(jobidlist[0]).name +'.replace '
 
 
