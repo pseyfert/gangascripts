a0bdd6c730dbf3916b10d93def400ee28677febc
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-05-04 15:10:49 +0200

    after testing

--- removeremote.py
+++ removeremote.py
@@ -25,13 +25,20 @@ def move_lfn_to_eos(lfn):
   if 'CERN-USER' in reps.keys():
      for key in reps.keys():
         if key != 'CERN-USER':
-            lfn.removeReplica(key)
+            print "there is still a replica at ",key
+            print "removal atm not implemented"
 
 def move_job_to_eos(job):
     ds = LHCbDataset()
     if type(job.backend)==Dirac:
         for sj in job.subjobs:
             if sj.status == 'completed':
-              ds.extend(sj.backend.getOutputDataLFNs())
+                for of in sj.outputfiles:
+                    try:
+                        print "fixing ", of.lfn
+                    except AttributeError:
+                        pass
+                    else:
+                        ds.extend(of)
         for lfn in ds:
             move_lfn_to_eos(lfn)
